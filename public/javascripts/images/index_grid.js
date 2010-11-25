cf45f02f8c1250141fdfc236775b4107e008aef9
Teruki Shigitani <shigitani@cybertech.co.jp>
2010-11-25 11:54:31 +0900

    use Ext.extend for images/index_grid.js

--- public/javascripts/images/index_grid.js
+++ public/javascripts/images/index_grid.js
@@ -1,110 +1,116 @@
-Images.IndexGrid = function() {
+Images.IndexGrid = Ext.extend(Ext.grid.GridPanel, {
 
-    var colModel = new Ext.grid.ColumnModel([
-	{
-	    header: 'ID',
-	    dataIndex: 'id',
-	    width: 30,
-	    sortable: true
-	},
-	{
-	    header: 'Title',
-	    dataIndex: 'title',
-	    width: 200,
-	    sortable: true
-	},
-	{
-	    header: 'OS',
-	    dataIndex: 'os',
-	    width: 150,
-	    sortable: true
-	},
-	{
-	    header: 'IQN',
-	    dataIndex: 'iqn',
-	    width: 450,
-	    sortable: true
-	},
-	{
-	    header: 'Comment',
-	    dataIndex: 'comment',
-	    width: 250
-	}
-    ]);
-
-    var store = itemsStore(paths.images.index, [
-	'id',
-	'title',
-	'os',
-	'iqn',
-	'comment',
-	'paths'
-    ]);
-
-    var contextMenu = new Ext.menu.Menu({
-	style: {
-	    overflow: 'visible'
-	},
-	items: [
-	    {
-		text: 'Edit',
-		handler: function() {
-		    grid.updateImage();
+    constructor: function() {
+	this.contextMenu = this.makeContextMenu();
+	Images.IndexGrid.superclass.constructor.call(this, {
+	    colModel: this.makeColModel(),
+	    store: this.makeStore(),
+	    autoHeight: true,
+	    listeners: {
+		rowcontextmenu: function(grid, row, e) {
+		    this.getSelectionModel().selectRow(row);
+		    e.stopEvent();
+		    this.contextMenu.showAt(e.getXY());
 		}
+	    }
+	});
+	this.getStore().load();
+    },
+
+    makeColModel: function() {
+	return new Ext.grid.ColumnModel([
+	    {
+		header: 'ID',
+		dataIndex: 'id',
+		width: 30,
+		sortable: true
 	    },
 	    {
-		text: 'Destroy',
-		handler: function() {
-		    grid.destroyImage();
+		header: 'Title',
+		dataIndex: 'title',
+		width: 200,
+		sortable: true
+	    },
+	    {
+		header: 'OS',
+		dataIndex: 'os',
+		width: 150,
+		sortable: true
+	    },
+	    {
+		header: 'IQN',
+		dataIndex: 'iqn',
+		width: 450,
+		sortable: true
+	    },
+	    {
+		header: 'Comment',
+		dataIndex: 'comment',
+		width: 250
+	    }
+	]);
+    },
+
+    makeStore: function() {
+	return itemsStore(paths.images.index, [
+	    'id',
+	    'title',
+	    'os',
+	    'iqn',
+	    'comment',
+	    'paths'
+	]);
+    },
+
+    makeContextMenu: function() {
+	var grid = this;
+	return new Ext.menu.Menu({
+	    style: {
+		overflow: 'visible'
+	    },
+	    items: [
+		{
+		    text: 'Edit',
+		    handler: function() {
+			grid.updateImage();
+		    }
+		},
+		{
+		    text: 'Destroy',
+		    handler: function() {
+			grid.destroyImage();
+		    }
 		}
-	    }
-	]
-    });
+	    ]
+	});
+    },
 
-    Images.IndexGrid.baseConstructor.apply(this, [{
-	colModel: colModel,
-	store: store,
-	autoHeight: true,
-	listeners: {
-	    rowcontextmenu: function(g, row, e) {
-		grid.getSelectionModel().selectRow(row);
-		e.stopEvent();
-		contextMenu.showAt(e.getXY());
-	    }
-	}
-    }]);
+    selectedId: function() {
+	return this.getSelectionModel().getSelected().get('id');
+    },
 
-    var grid = this;
+    selectedPaths: function() {
+	return this.getSelectionModel().getSelected().get('paths');
+    },
 
-    store.load();
-
-    this.selectedId = function() {
-	return grid.getSelectionModel().getSelected().get('id');
-    };
-
-    this.selectedPaths = function() {
-	return grid.getSelectionModel().getSelected().get('paths');
-    };
-
-    this.addRecord = function(item) {
+    addRecord: function(item) {
+	var store = this.getStore();
 	var RecordType = store.recordType;
 	var record = new RecordType(item);
 	store.add(record);
-    };
+    },
 
-    this.updateSelectedValues = function(item) {
+    updateSelectedValues: function(item) {
 	var record = grid.getSelectionModel().getSelected();
 	for (var field in item) {
 	    record.set(field, item[field]);
 	    record.commit();
 	}
-    };
+    },
 
-    this.removeSelected = function() {
+    removeSelected: function() {
 	var record = grid.getSelectionModel().getSelected();
-	store.remove(record);
-    };
+	this.getStore().remove(record);
+    }
 
-};
-
-Images.IndexGrid.inherit(Ext.grid.GridPanel);
+});
